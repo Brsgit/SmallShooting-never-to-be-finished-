596239b1e7a6e48c8c5f93a4c66f591cd421bcc1
Brsgit <rod.bakeev@yandex.ru>
2022-04-16 14:28:55 +0300

    InputController added

---
+++ Assets/Scripts/Core/InputController.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    public event Action OnTouch;
+
+    private float _nextInput = 0;
+
+    private const float TOUCH_COOLDAWN = 1.5f;
+
+    public static InputController Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Update()
+    {
+        _nextInput += Time.deltaTime;
+        if (Input.GetButtonDown("Fire1") && _nextInput > TOUCH_COOLDAWN)
+        {
+            _nextInput -= TOUCH_COOLDAWN;
+            OnTouch?.Invoke();
+        }
+    }
+}

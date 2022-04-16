ec14604728ac93b9ab79317cdd3f8d76be0d766d
Brsgit <rod.bakeev@yandex.ru>
2022-04-16 14:44:30 +0300

    Waypoints added

--- Assets/Scripts/Navigation/WaypointsContainer.cs
+++ Assets/Scripts/Navigation/WaypointsContainer.cs
@@ -1,15 +1,21 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Navigation
 {
     public class WaypointsContainer : MonoBehaviour
     {
-        [SerializeField] private List<IDestination> _waypoints;
+        private List<IDestination> _waypoints;
 
         private int _nextWaypoint = 0;
 
         public IEnumerable<IDestination> Waypoints => _waypoints;
+
+        private void Start()
+        {
+            _waypoints = GetComponentsInChildren<IDestination>().ToList();
+        }
 
         public IDestination GetNextWaypoint()
         {
